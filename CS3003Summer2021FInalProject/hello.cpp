2069f39c69167a380425f84771172b544764ed61
stankyBagel42 <ericwolff42@gmail.com>
2021-08-04 14:30:56 -0400

    tabs->spaces

--- CS3003Summer2021FInalProject/hello.cpp
+++ CS3003Summer2021FInalProject/hello.cpp
@@ -1,15 +1,15 @@
 // a first program with
 // two comment lines
 int main() {
-    int j;    
-	int m;
-	long z;
-	m=2;
-	m=m^2;
-	z=4141;
+    int j;
+    int m;
+    long z;
+    m=2;
+    m=m^2;
+    z=4141;
     for(j=0;j<=10;j=j+1;)
     {
-	m=m^j;
-	m=m+1;
+    m=m^j;
+    m=m+1;
     }
 } // main
